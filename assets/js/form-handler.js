478b27cb862da4d263fff038c44802414ff48343
John Vincent <corcegajovic@gmail.com>
2021-04-02 14:58:30 +0800

    Submit form using Netlify AJAX

--- assets/js/form-handler.js
+++ assets/js/form-handler.js
@@ -1,5 +1,6 @@
 window.addEventListener("DOMContentLoaded", function () {
   var form = document.getElementById("contact-form");
+  var myForm = document.getElementById("contact-form");
   var button = document.getElementById("contact-form-button");
   var status = document.getElementById("contact-form-status");
 
@@ -18,24 +19,17 @@ window.addEventListener("DOMContentLoaded", function () {
     form.addEventListener("submit", function (ev) {
       ev.preventDefault();
       var data = new FormData(form);
-      ajax(form.method, form.action, data, success, error);
+      ajax(form.method, data, success, error);
     });
   }
 });
 
 // helper function for sending an AJAX request
-
-function ajax(method, url, data, success, error) {
-  var xhr = new XMLHttpRequest();
-  xhr.open(method, url);
-  xhr.setRequestHeader("Accept", "application/json");
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState !== XMLHttpRequest.DONE) return;
-    if (xhr.status === 200) {
-      success(xhr.response, xhr.responseType);
-    } else {
-      error(xhr.status, xhr.response, xhr.responseType);
-    }
-  };
-  xhr.send(data);
+function ajax(method, data, success, error) {
+  fetch('/', {
+    method: 'POST',
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: new URLSearchParams(data).toString()
+  }).then(() => success()).catch((error) => {
+    console.log(error); error()})
 }
