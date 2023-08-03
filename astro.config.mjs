53e67c33b9e0c5ccbffa2c987f58e25553d9f584
John Vincent <git@tenseventyseven.cf>
2023-08-03 16:27:58 +0800

    chore: set proper website address

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import { astroImageTools } from 'astro-imagetools';
 // https://astro.build/config
 export default defineConfig({
   // base: '.', // Set a path prefix.
-  site: 'https://example.com/', // Use to generate your sitemap and canonical URLs in your final build.
+  site: 'https://tenseventyseven.cf/', // Use to generate your sitemap and canonical URLs in your final build.
   trailingSlash: 'always', // Use to always append '/' at end of url
   // Important!
   // Only official '@astrojs/*' integrations are currently supported by Astro.
